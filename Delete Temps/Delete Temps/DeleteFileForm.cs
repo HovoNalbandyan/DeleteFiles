613f1ec129dff4b3b97313fd79270768f8a74204
nalbandyan.1991 <nalbandyan.1991@bk.ru>
2017-02-02 15:37:03 +0400

    New Version

--- Delete Temps/Delete Temps/DeleteFileForm.cs
+++ Delete Temps/Delete Temps/DeleteFileForm.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.IO;
+
+
 namespace Delete_Temps
 {
     public partial class DeleteFileForm : Form
@@ -24,25 +27,60 @@ namespace Delete_Temps
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
-           
-        }
+            DirectoryInfo di = new DirectoryInfo(path);
+            try
+            {
+                if (textBox3.Text == textBox2.Text)
+                {
+                    FileInfo[] arr = di.GetFiles();
+                    foreach (FileInfo file in arr)
+                    {
+                        file.Delete();
+                    }
+                }
 
+            }
+            catch
+            {
+                throw new Exception("Input Rigt Random Password");
+
+            }
+
+
+           
+            
+            
+
+        }
+        string path;
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            Console.WriteLine("afa");
+            
+            path = textBox1.Text;
+            if (path != null)
+            {
+                double[] x = new double[3000];
+                Random rnd = new Random();
+                textBox3.Text = rnd.Next(1000, 3000).ToString();
+            }
+
         }     
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
 
+
+
         }
+
         private void textBox3_TextChanged(object sender, EventArgs e)
-        {
-            
+        { 
+            //random generator
+           
 
-              
+
 
         }
+
     }
 }
--- Delete Temps/Delete Temps/Program.cs
+++ Delete Temps/Delete Temps/Program.cs
@@ -1,45 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using System.IO;
+using System.Windows.Forms;
 
 namespace Delete_Temps
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            
-            //Console.WriteLine("Do you want to delete temps in C Disc?");
-            //Console.Write("Press write  Delete =");
-            string str2= Console.ReadLine();
-            string str1 = "Random Numbers";
-            string str3;
-            int n = 3000;
-            double[] x = new double[n];
-            Random rnd = new Random();
-            str3 = rnd.Next(1000, 3000).ToString();
-            Console.WriteLine(str3);
-            DirectoryInfo di = new DirectoryInfo(@"C:\Users\Anahit\Desktop\Desctop\temps\");
-
-            try
-            {
-                if (str3 == str1)
-
-                {
-                    FileInfo[] arr = di.GetFiles();
-                    foreach (FileInfo file in arr)
-                    {
-                        file.Delete();
-                    }
-                }
-            }
-            catch
-            {
-                //throw new FieldAccessException();
-            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new DeleteFileForm());
         }
     }
 }
